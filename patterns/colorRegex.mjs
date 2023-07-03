9cf565e71843a4367ba19b3ea46ac36ee1c537a6
Clayton Ingalls <github@zapbampow.com>
2023-07-03 14:38:35 -0400

    refactor(color regex): begins adding variables for repeated patterns
    - refactors hsl and rgb regex to use repeating patterns

--- patterns/colorRegex.mjs
+++ patterns/colorRegex.mjs
@@ -1,12 +1,36 @@
+const decimal = `(?:\\.\\d+)`;
+const zeroToOne = `(?:0|0?(${decimal})?|1)`;
+const zeroToNine = `[0-9]${decimal}?`;
+const oneToNine = `[1-9]${decimal}?`;
+const zeroToTen = `(${zeroToNine}|10)`;
+const zeroToNinetyNine = `([0-9]${decimal}?|[1-9][0-9]${decimal}?)`;
+const zeroToOneHundred = `(${zeroToNinetyNine}|100)`;
+const zeroToOneHundredPercent = `${zeroToOneHundred}%`;
+const zeroToTwoHundredFiftyFive = `(0?${decimal}|0|${oneToNine}|[1-9][0-9]${decimal}?|1[0-9][0-9]${decimal}?|2[0-4][0-9]${decimal}?|25[0-4]${decimal}?|255)`;
+const hue = `((0|0?${decimal}|-?[1-9]([0-9]+)?${decimal}?)((deg)|(g?rad)|(turn))?)`;
+console.log("zeroToTwoHundredFiftyFive", zeroToTwoHundredFiftyFive);
+
 const hexRegex = /^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-const hslCommas =
-  /^(hsl)a?\((?<hue>(-|\+)?((0|[1-9]([0-9]+)?)|(?:(0|[1-9]+)?\.\d+)?)((deg)|(g?rad)|(turn))?), ?(?<saturation>(\b([0-9]|[1-9][0-9]|100)\b)(?:\.\d+)?%, ?)(?<light>(\b([0-9]|[1-9][0-9]|100)\b)(?:\.\d+)?%,? ?)(?<alpha>((\b([0-9]|[1-9][0-9]|100)\b)(?:\.\d+)?%)|(0?(?:\.\d+))|1)?\)$/i;
-const hslSpaces =
-  /^(hsl)a?\((?<hue>(-|\+)?((0|[1-9]([0-9]+)?)|(?:(0|[1-9]([0-9]+)?)?\.\d+)?)((deg)|(g?rad)|(turn))?) (?<saturation>\b((100)|[1-9][0-9]|[0-9])\b(\.\d+)?%) (?<lightness>\b((100)|[1-9][0-9]|[0-9])\b(\.\d+)?%)( \/ (?<alpha>((\b(0|[1-9]([0-9])?|100)\b)(?:\.\d+)?%)|(0?(?:\.\d+))|1))?\)$/i;
-const rgbCommas =
-  /^rgba?\((((?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)|((?:100|\d{1,2})%))((, )|,)){2}(((?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)|((?:100|\d{1,2})%)))((, ?)(((?:100|\d{1,2})%)|(0?\.\d+%?)))?\)/;
-const rgbSpaces =
-  /^rgba?\((((?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)|((?:100|\d{1,2})%)) ){2}(((?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)|((?:100|\d{1,2})%)))( \/ (((?:100|\d{1,2})%)|(0?\.\d+%?)))?\)/;
+
+// hsl done only for commas
+const hslC = `^(hsl)a?\\(${hue}, ?(${zeroToOneHundredPercent}), ?(${zeroToOneHundredPercent})(, ?(${zeroToOneHundredPercent}|${zeroToOne}))?\\)$`;
+const hslCommas = new RegExp(hslC, "i");
+
+// hsl done only for spaces
+const hslS = `^(hsl)a?\\(${hue} (${zeroToOneHundredPercent}) (${zeroToOneHundredPercent})( \\/ (${zeroToOneHundredPercent}|${zeroToOne}))?\\)$`;
+const hslSpaces = new RegExp(hslS, "i");
+
+const rgbC = `^(rgb)a?\\((${zeroToTwoHundredFiftyFive}), ?(${zeroToTwoHundredFiftyFive}), ?(${zeroToTwoHundredFiftyFive})(, ?(${zeroToOneHundredPercent}|${zeroToOne}))?\\)$`;
+const rgbCommas = new RegExp(rgbC, "i");
+
+const rgbS = `^(rgb)a?\\((${zeroToTwoHundredFiftyFive}) (${zeroToTwoHundredFiftyFive}) (${zeroToTwoHundredFiftyFive})( \\/ (${zeroToOneHundredPercent}|${zeroToOne}))?\\)$`;
+const rgbSpaces = new RegExp(rgbS, "i");
+
+const rgbP = `^(rgb)a?\\((${zeroToOneHundredPercent}) (${zeroToOneHundredPercent}) (${zeroToOneHundredPercent})( \\/ (${zeroToOneHundredPercent}|${zeroToOne}))?\\)$`;
+const rgbPercents = new RegExp(rgbP, "i");
+
+// const rgbSpaces =
+//   /^rgba?\((((?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)|((?:100|\d{1,2})%)) ){2}(((?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)|((?:100|\d{1,2})%)))( \/ (((?:100|\d{1,2})%)|(0?\.\d+%?)))?\)/;
 const labRegex =
   /^lab\((0?(?:100|\d{0,2}(?:\.\d*)?)%?)( -?((0?(?:100|\d{0,2}(?:\.\d*)?)%?)|((0?[0-9]?[0-9]|1[01][0-9]|12[0-5])))){2}( \/ (((?:100|\d{1,2})%)|(0?\.\d+%?)))?\)/;
 const oklabRegex =
@@ -22,6 +46,7 @@ export {
   hslSpaces,
   rgbCommas,
   rgbSpaces,
+  rgbPercents,
   labRegex,
   oklabRegex,
   lchRegex,
--- validate/rgb.mjs
+++ validate/rgb.mjs
@@ -1,9 +1,10 @@
-import { rgbCommas, rgbSpaces } from "../patterns/colorRegex.mjs";
+import { rgbCommas, rgbSpaces, rgbPercents } from "../patterns/colorRegex.mjs";
 import noColor from "./noColor.mjs";
 
 export default function isRgb(color) {
   if (noColor(color)) return false;
   let commas = rgbCommas.test(color);
   let spaces = rgbSpaces.test(color);
-  return commas || spaces;
+  let percent = rgbPercents.test(color);
+  return commas || spaces || percent;
 }
